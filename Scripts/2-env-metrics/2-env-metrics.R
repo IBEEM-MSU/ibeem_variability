aba90a1f74c74fb62f595d9f02a73a801c6e7a66
Casey Youngflesh <caseyyoungflesh@gmail.com>
2023-05-20 16:02:47 -0400

    add spectral analysis

--- Scripts/2-env-metrics/2-env-metrics.R
+++ Scripts/2-env-metrics/2-env-metrics.R
@@ -22,6 +22,7 @@ args <- commandArgs(trailingOnly = TRUE)
 library(tidyverse)
 library(data.table)
 library(moments)
+library(lomb)
 
 
 # read in data -------------------------------------------------
@@ -32,10 +33,12 @@ env_out <- read.csv(args[1])
 # Env metrics -------------------------------------------------------------
 
 #metrics:
+#-mean
 #-slope over time (and se)
 #-variance (or sd) of residuals (i.e., variability of detrended time series)
 #-kurtosis (i.e., heavy-tailed ness - or model time series using t-distribution and estimate degrees of freedom parameter)
 #-skew (i.e., skew of residuals)
+#-spectral exponent ('color' of noise - 0 <= B <= 0.5 = white, 0.5 < B <= 1.5 = red, 1.5 < brown <= 2)
 #-temporal autocorrelation (of residuals - see Leung et al. 2020 Eco Letters for use of this metric for 'predictability')
 
 #add unique cell id
@@ -59,6 +62,8 @@ env_df <- data.frame(cell_id = rep(uci, each = 2),
                      sd_resid = NA, #sd of residuals for linear model
                      kurt = NA, #kurtosis
                      skew = NA, #skewness
+                     spectral_alpha = NA, #intercept of log10(power) ~ log10(freq)
+                     spectral_beta = NA, #spectral exponent
                      rho_l1 = NA, #temporal autocorrelation lag 1
                      rho_l2 = NA, #lag 2
                      rho_l3 = NA,
@@ -84,6 +89,20 @@ for (i in 1:length(uci))
   temp_resid <- residuals(fit_temp)
   precip_resid <- residuals(fit_precip)
   
+  #spectral analysis using Lomb-Scargle Periodogram
+  #between freq 2/(n*dt) and 1/(2*dt), where dt = 1 and n = 72; 0.0278 to 0.5
+  #following Marshall and Burgess 2015 Eco Letters
+  #see also Vasseur and Yodzis 2004 Ecology
+  #period = 1/freq; ~2 - 36 years
+  temp_spec <- lomb::lsp(temp_resid, from = 0.0278, to = 0.5, type = 'frequency',
+                         normalize =  'standard', plot = FALSE)
+  pecip_spec <- lomb::lsp(precip_resid, from = 0.0278, to = 0.5, type = 'frequency',
+                         normalize =  'standard', plot = FALSE)
+  
+  #spectral exponent (1/f^beta)
+  temp_spec_fit <- summary(lm(log10(temp_spec$power) ~ log10(temp_spec$scanned)))
+  precip_spec_fit <- summary(lm(log10(precip_spec$power) ~ log10(precip_spec$scanned)))
+  
   #temporal autocorrelation
   temp_acf <- acf(temp_resid, lag.max = 5, plot = FALSE)
   precip_acf <- acf(precip_resid, lag.max = 5, plot = FALSE)
@@ -92,11 +111,19 @@ for (i in 1:length(uci))
   env_df$lon[counter:(counter + 1)] <- rep(te$lon[1], 2)
   env_df$lat[counter:(counter + 1)] <- rep(te$lat[1], 2)
   env_df$mean[counter:(counter + 1)] <- c(mean(te$temp), mean(te$precip))
-  env_df$slope[counter:(counter + 1)] <- c(fit_temp$coefficients[2,1], fit_precip$coefficients[2,1])
-  env_df$se_slope[counter:(counter + 1)] <- c(fit_temp$coefficients[2,2], fit_precip$coefficients[2,2])
+  env_df$slope[counter:(counter + 1)] <- c(fit_temp$coefficients[2,1], 
+                                           fit_precip$coefficients[2,1])
+  env_df$se_slope[counter:(counter + 1)] <- c(fit_temp$coefficients[2,2], 
+                                              fit_precip$coefficients[2,2])
   env_df$sd_resid[counter:(counter + 1)] <- c(sd(temp_resid), sd(precip_resid))
-  env_df$kurt[counter:(counter + 1)] <- c(moments::kurtosis(temp_resid), moments::kurtosis(precip_resid))
-  env_df$skew[counter:(counter + 1)] <- c(moments::skewness(temp_resid), moments::skewness(precip_resid))
+  env_df$kurt[counter:(counter + 1)] <- c(moments::kurtosis(temp_resid), 
+                                          moments::kurtosis(precip_resid))
+  env_df$skew[counter:(counter + 1)] <- c(moments::skewness(temp_resid), 
+                                          moments::skewness(precip_resid))
+  env_df$spectral_alpha[counter:(counter + 1)] <- c(temp_spec_fit$coefficients[1,1], 
+                                                   precip_spec_fit$coefficients[1,1])
+  env_df$spectral_beta[counter:(counter + 1)] <- c(temp_spec_fit$coefficients[2,1] * -1, 
+                                                   precip_spec_fit$coefficients[2,1] * -1)
   env_df$rho_l1[counter:(counter + 1)] <- c(temp_acf$acf[2], precip_acf$acf[2])
   env_df$rho_l2[counter:(counter + 1)] <- c(temp_acf$acf[3], precip_acf$acf[3])
   env_df$rho_l3[counter:(counter + 1)] <- c(temp_acf$acf[4], precip_acf$acf[4])
