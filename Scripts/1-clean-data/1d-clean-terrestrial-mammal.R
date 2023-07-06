0a9657ba88e29f1c3e9b035fc4bacb3b7da3eb7f
kellykapsar <kelly.kapsar@gmail.com>
2023-07-05 19:18:32 -0500

    Scripts to clean terrestrial mammal data and extract environmental covariates (not optimized for HPCC yet).

---
+++ Scripts/1-clean-data/1d-clean-terrestrial-mammal.R
@@ -0,0 +1,55 @@
+################
+# Combine/clean bird gen time from Bird et al. 2020 Conservation Biology
+#
+################
+
+
+# specify dir -------------------------------------------------------------
+
+#path KK machine
+# dir <- '~/Google_Drive/Research/Projects/IBEEM_variabilty/'
+dir <- "../terrestrial-mammal/MAMMALS_TERRESTRIAL_ONLY.shp"
+
+# load packages -----------------------------------------------------------
+
+library(tidyverse)
+library(sf)
+
+
+# read in data ------------------------------------------------------------
+
+mam <- st_read(dir)
+
+
+# combine -----------------------------------------------------------------
+
+
+# Select known areas of occurrence 
+# Select only resident spp
+# Select native spp 
+# See IUCN_Standard_attributes_for_spatial_data-v1.19_2021.xlsx for 
+# "IUCN Codes" tab for other code meanings
+
+mam1 <- mam %>% filter(presence == 1, seasonal == 1, origin == 1)
+
+#### Testing #### 
+# sci_name and id_no both unique identifiers for each species 
+# test <- unique(mam[,c("sci_name", "id_no")])
+# length(unique(test$sci_name))
+# length(unique(test$id_no))
+#################
+
+# Merge species with multiple range polys into one multipoly 
+# i.e. one row per species 
+# A lot of the duplicate rows seem to be spp that live on multiple islands
+mam2 <- mam1 %>% 
+  rename(order = order_) %>% 
+  group_by(sci_name, id_no, kingdom, phylum, class, order, family, genus) %>% 
+  summarize(geometry = st_combine(geometry))
+
+
+# write out ---------------------------------------------------------------
+
+st_write(mam2, "../terrestrial-mammal/terrestrial-mammals-clean.shp")
+
+          
---
+++ Scripts/4-extract-species-env/4b-extract-avg-within-range-terrestrial-mammal.R
@@ -0,0 +1,78 @@
+# 4-example-species-extract.R: a script to start thinking about how we'll want
+#                              to calculate metrics within each species range
+#                              and output all the associated information for 
+#                              running models of the form trait ~ climate
+rm(list = ls())
+library(tidyverse)
+library(sf)
+
+# Specify directory -------------------------------------------------------
+mam.dir <- '../terrestrial-mammal/'
+env.dir <- '../env/'
+out.dir <- '../env_out/'
+
+# Get the current file to process -----------------------------------------
+# file.name <- commandArgs(trailingOnly = TRUE)
+# Testing
+# file.name <- 'BLIDsPiece-14.rda'
+# if(length(file.name) == 0) base::stop('Need to give the file name to process')
+
+# Read in data ------------------------------------------------------------
+# Loads in current set of ids (a vector called ids)
+mam <- st_read(paste0(mam.dir, "terrestrial-mammals-clean.shp"))
+# Climate data (takes a couple of minutes to load
+# NOTE: can change this to the GAM stuff if that's what we want.
+env.dat <- read.csv(paste0(env.dir, 'Env-var-1_2_3_4_5_6_7_8_9_10_11_12.csv'))
+# Convert to sf
+env.dat.sf <- st_as_sf(env.dat, 
+		       coords = c('lon', 'lat'),
+		       crs = 4326)
+
+# Loop through all the species in the current file ------------------------
+# Make data frame to hold everything
+# Number of climate variables
+clim.var.names <- sort(unique(env.dat$var))
+n.clim.var <- length(clim.var.names)
+# Different metrics of the variables
+metrics <- env.dat %>%
+  dplyr::select(-cell_id, -var, -lon, -lat) %>%
+  colnames()
+n.metrics <- length(metrics)
+avg.clim.df <- matrix(NA, nrow = length(mam$sci_name), ncol = n.metrics * n.clim.var + 1)
+# Definitely a better way to do this
+tmp.names <- paste(rep(metrics, each = n.clim.var), clim.var.names, sep = "_")
+colnames(avg.clim.df) <- c('id', tmp.names)
+id.indx <- which(colnames(avg.clim.df) == 'id')
+avg.clim.df <- as.data.frame(avg.clim.df)
+
+start <- proc.time()			  
+for (i in 1:5) {
+# for (i in 1:length(mam$sci_name)) {
+  print(paste0("Currently on species ", i, " out of ", length(mam$sci_name)))
+  curr.sp <- mam$sci_name[i]  
+  curr.range <- mam[mam$sci_name ==  mam$sci_name[i],]
+  # Get pixels within current species range. Takes about 2 min per species
+  env.curr.range <- st_crop(env.dat.sf, curr.range)
+  # Get average of all the environmental variables for the given species
+  bad.cols <- which(names(env.curr.range) %in% c('cell_id', 'geometry'))
+  avg.clim.vars <- env.curr.range %>%
+    dplyr::select(-cell_id, -geometry) %>%
+    st_drop_geometry() %>%
+    group_by(var) %>%
+    summarize(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>%
+    arrange(var) %>%
+    dplyr::select(-var) %>%
+    as.matrix() %>%
+    c()
+  avg.clim.df[i, id.indx] <- curr.sp
+  # Need this if, as there are some species whose ranges don't overlap the climate
+  # data (e.g., Snares Penguin)
+  if (length(avg.clim.vars) > 0) {
+    avg.clim.df[i, -id.indx] <- avg.clim.vars
+  }
+  rm(curr.range, env.curr.range)
+  gc()
+}
+proc.time() - start
+save(avg.clim.df, file = paste0(out.dir, 'summarized-data-piece-', 
+				str_extract(file.name, '\\d+')))
