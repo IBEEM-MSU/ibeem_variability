f233cce126aa76e27f1d509eb4d1679b17f409e1
Casey Youngflesh <caseyyoungflesh@gmail.com>
2023-08-17 11:06:30 -0400

    add rasterize range script (notes only)

---
+++ Scripts/X-rasterize-ranges.R
@@ -0,0 +1,7 @@
+#stack bird range rasters (with gen time) and make plots
+
+#rasterize range map
+
+#see here: https://github.com/RS-eco/rasterSp
+
+#could also use st_make_grid in sf or possibly turn raster into sf object to do this
