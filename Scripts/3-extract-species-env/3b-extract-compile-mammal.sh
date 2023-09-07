941b4dcbdc752c8bdd17973644b384a2639b93d1
kellykapsar <kelly.kapsar@gmail.com>
2023-09-07 11:44:10 -0500

    Update number of pieces.

--- Scripts/3-extract-species-env/3b-extract-compile-mammal.sh
+++ Scripts/3-extract-species-env/3b-extract-compile-mammal.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 n="es" # job name
 
-# Note that 52 is the number of pieces the MAM IDs were split up into. 
+# Note that 54 is the number of pieces the MAM IDs were split up into. 
 # Should probably not hardcode this...
-for i in {1..52}
+for i in {1..54}
 do
   sbatch --job-name=$n.$i --output=$n.$i.SLURMout --export=fileName=MAMIDsPiece-$i.rda 3b-extract-mammal.SB
 done
