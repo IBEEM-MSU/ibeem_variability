a613487b9ea635a59a5f3923a96ff9f74abc2d62
kellykapsar <kelly.kapsar@gmail.com>
2023-09-07 11:37:02 -0500

    Clean up code organization and make it so the script can be run without an IUCN red list key.

--- Scripts/1-clean-data/1d-taxonomic-harmonization-terrestrial-mammal.R
+++ Scripts/1-clean-data/1d-taxonomic-harmonization-terrestrial-mammal.R
@@ -47,110 +47,115 @@ MAM_data <- sf::st_read(dsn = paste0(range_map_data_dir, 'terrestrial-mammals-cl
 
 #mammal life history data
 #data location: https://drive.google.com/drive/folders/11eAFmFKUc7tU59IArNEpN5YP_ehGvAwZ?usp=drive_link
+# Life history data from Pacifici (LH)
 LH_data <- read.csv(paste0(life_history_dir, 'Generation Length for Mammals.csv')) %>% 
   rename(name_pacifici = Scientific_name)
 
+# Name data for phylacine (NM)
 NM_data <- read.csv(paste0(life_history_dir, 'phylacine_data/Synonymy_table_valid_species_only.csv'))
 
-PH_data <- read.csv(paste0(life_history_dir, 'phylacine_data/Trait_data.csv')) %>% 
-  mutate(name_phylacine = gsub("_", " ", Binomial.1.2)) %>% select(-Binomial.1.2)
+# ID name inconsistencies between range and trait dbs and fix ------------------------------------
 
+# This section only needs to be run once to generate mammal-names-matched.csv and mammal-names-no-match.csv
+# These names were then manually reconciled and saved as mammal-names-fixed.csv 
+# mammal-names-matched.csv and mammal-names-fixed.csv are later loaded into the script to 
+# continue the process. 
 
-NM <- NM_data %>% 
-  mutate(name_phylacine = paste(Genus.1.2, Species.1.2),
-         name_phylacine_1.1 = paste(Genus.1.1, Species.1.1),
-         name_phylacine_1.0 = paste(Genus.1.0, Species.1.0),
-         name_EltonTraits = paste(EltonTraits.1.0.Genus, EltonTraits.1.0.Species),
-         name_iucn_2016 = paste(IUCN.2016.3.Genus, IUCN.2016.3.Species)) %>% 
-  mutate (name_phylacine = gsub("000 Species not accepted 000 Species not accepted", NA, name_phylacine),
-          name_phylacine_1.1 = gsub("000 Species not accepted 000 Species not accepted", NA, name_phylacine_1.1),
-          name_phylacine_1.0 = gsub("000 Species not accepted 000 Species not accepted", NA, name_phylacine_1.0),
-          name_EltonTraits = gsub("000 Species not accepted 000 Species not accepted", NA, name_EltonTraits),
-          name_iucn_2016 = gsub("000 Species not accepted 000 Species not accepted", NA, name_iucn_2016)) %>% 
-  select(name_phylacine,
-         name_phylacine_1.1,
-         name_phylacine_1.0,
-         name_EltonTraits,
-         name_iucn_2016)
-
-LH <- LH_data %>% 
-  select(name_pacifici) 
-
-MAM <- MAM_data %>% 
-  st_drop_geometry() %>% 
-  select(name_iucn) %>% 
-  mutate(id = 1:n_distinct(name_iucn)) 
-
-# Join data, use Phylacine synonyms to match names
-t <- MAM %>% mutate("name_phylacine" = NA,
-                    "name_pacifici" = NA)
-for(i in 1:nrow(t)){
-  j <- ifelse(t$name_iucn[i] %in% NM$name_phylacine,
-              which(t$name_iucn[i] == NM$name_phylacine),
-              ifelse(t$name_iucn[i] %in% NM$name_phylacine_1.1,
-                     which(t$name_iucn[i] == NM$name_phylacine_1.1),
-                     ifelse(t$name_iucn[i] %in% NM$name_phylacine_1.0,
-                            which(t$name_iucn[i] == NM$name_phylacine_1.0),
-                            ifelse(t$name_iucn[i] %in% NM$name_EltonTraits,
-                                   which(t$name_iucn[i] == NM$name_EltonTraits),
-                                   NA))))
-  t$name_phylacine[i] <- ifelse(is.na(j), NA,
-                                NM$name_phylacine[j])
-}
-t <- left_join(t, NM, by = "name_phylacine")
-for(i in 1:nrow(t)){
-  t$name_pacifici[i] <- ifelse(t$name_iucn[i] %in% LH$name_pacifici,
-                               t$name_iucn[i],
-                               ifelse(t$name_phylacine[i] %in% LH$name_pacifici,
-                                      t$name_phylacine[i],
-                                      ifelse(t$name_phylacine_1.1[i] %in% LH$name_pacifici,
-                                             t$name_phylacine_1.1[i],
-                                             ifelse(t$name_phylacine_1.0[i] %in% LH$name_pacifici,
-                                                    t$name_phylacine_1.0[i],
-                                                    ifelse(t$name_EltonTraits[i] %in% LH$name_pacifici,
-                                                           t$name_EltonTraits[i], NA)))))
-}
-t <- t %>% select(name_iucn, name_phylacine, name_pacifici)
-
-# Use IUCN Red List synonyms to match names
-# Need IUCN Red List key to access API
-t_complete <- t[rowSums(is.na(t)) == 0,]
-t_no_match <- t[rowSums(is.na(t)) > 0,]
-sum(!is.na(t_no_match$name_iucn))
-
-pb <- progress_bar$new(
-  format = "[:bar] :percent in :elapsed with :eta remaining",
-  total = nrow(t_no_match), clear = FALSE, width= 60)
-for(i in 1:nrow(t_no_match)){
-  syn <- rredlist::rl_synonyms(name = t_no_match$name_iucn[i],
-                               key = IUCN_REDLIST_KEY,
-                               parse = TRUE)$result
-  new_name_phylacine <- ifelse(length(syn)==0,
-                               t_no_match$name_phylacine[i],
-                               syn$synonym[which(syn$synonym %in% NM$name_phylacine)])
-  new_name_pacifici <- ifelse(length(syn)==0,
-                              t_no_match$name_pacifici[i],
-                              syn$synonym[which(syn$synonym %in% LH$name_pacifici)])
-  t_no_match$name_phylacine[i] <- ifelse(is.na(t_no_match$name_phylacine[i]),
-                                         new_name_phylacine,
-                                         t_no_match$name_phylacine[i])
-  t_no_match$name_pacifici[i] <- ifelse(is.na(t_no_match$name_pacifici[i]),
-                                        new_name_pacifici,
-                                        t_no_match$name_pacifici[i])
-  pb$tick()
-}
-
-# Add newly matched names to list of complete names, keep unmatched names
-t_new_names <- t_no_match[rowSums(is.na(t_no_match)) == 0,]
-t_complete <- bind_rows(t_complete, t_new_names)
-t_no_match <- t_no_match[rowSums(is.na(t_no_match)) > 0,]
-sum(!is.na(t_no_match$name_iucn))
-
-# Save files
-write.csv(t_complete, file = paste0(out_dir, 'mammal-names-matched.csv'), row.names = FALSE)
-write.csv(t_no_match, file = paste0(out_dir, 'mammal-names-no-match.csv'), row.names = FALSE)
-write.csv(NM, file = paste0(out_dir, 'mammal-names-phylacine.csv'), row.names = FALSE)
-write.csv(LH, file = paste0(out_dir, 'mammal-names-pacifici.csv'), row.names = FALSE)
+# NM <- NM_data %>% 
+#   mutate(name_phylacine = paste(Genus.1.2, Species.1.2),
+#          name_phylacine_1.1 = paste(Genus.1.1, Species.1.1),
+#          name_phylacine_1.0 = paste(Genus.1.0, Species.1.0),
+#          name_EltonTraits = paste(EltonTraits.1.0.Genus, EltonTraits.1.0.Species),
+#          name_iucn_2016 = paste(IUCN.2016.3.Genus, IUCN.2016.3.Species)) %>% 
+#   mutate (name_phylacine = gsub("000 Species not accepted 000 Species not accepted", NA, name_phylacine),
+#           name_phylacine_1.1 = gsub("000 Species not accepted 000 Species not accepted", NA, name_phylacine_1.1),
+#           name_phylacine_1.0 = gsub("000 Species not accepted 000 Species not accepted", NA, name_phylacine_1.0),
+#           name_EltonTraits = gsub("000 Species not accepted 000 Species not accepted", NA, name_EltonTraits),
+#           name_iucn_2016 = gsub("000 Species not accepted 000 Species not accepted", NA, name_iucn_2016)) %>% 
+#   select(name_phylacine,
+#          name_phylacine_1.1,
+#          name_phylacine_1.0,
+#          name_EltonTraits,
+#          name_iucn_2016)
+# 
+# LH <- LH_data %>% 
+#   select(name_pacifici) 
+# 
+# MAM <- MAM_data %>% 
+#   st_drop_geometry() %>% 
+#   select(name_iucn) %>% 
+#   mutate(id = 1:n_distinct(name_iucn)) 
+# 
+# # Join data, use Phylacine synonyms to match names
+# t <- MAM %>% mutate("name_phylacine" = NA,
+#                     "name_pacifici" = NA)
+# for(i in 1:nrow(t)){
+#   j <- ifelse(t$name_iucn[i] %in% NM$name_phylacine,
+#               which(t$name_iucn[i] == NM$name_phylacine),
+#               ifelse(t$name_iucn[i] %in% NM$name_phylacine_1.1,
+#                      which(t$name_iucn[i] == NM$name_phylacine_1.1),
+#                      ifelse(t$name_iucn[i] %in% NM$name_phylacine_1.0,
+#                             which(t$name_iucn[i] == NM$name_phylacine_1.0),
+#                             ifelse(t$name_iucn[i] %in% NM$name_EltonTraits,
+#                                    which(t$name_iucn[i] == NM$name_EltonTraits),
+#                                    NA))))
+#   t$name_phylacine[i] <- ifelse(is.na(j), NA,
+#                                 NM$name_phylacine[j])
+# }
+# t <- left_join(t, NM, by = "name_phylacine")
+# for(i in 1:nrow(t)){
+#   t$name_pacifici[i] <- ifelse(t$name_iucn[i] %in% LH$name_pacifici,
+#                                t$name_iucn[i],
+#                                ifelse(t$name_phylacine[i] %in% LH$name_pacifici,
+#                                       t$name_phylacine[i],
+#                                       ifelse(t$name_phylacine_1.1[i] %in% LH$name_pacifici,
+#                                              t$name_phylacine_1.1[i],
+#                                              ifelse(t$name_phylacine_1.0[i] %in% LH$name_pacifici,
+#                                                     t$name_phylacine_1.0[i],
+#                                                     ifelse(t$name_EltonTraits[i] %in% LH$name_pacifici,
+#                                                            t$name_EltonTraits[i], NA)))))
+# }
+# t <- t %>% select(name_iucn, name_phylacine, name_pacifici)
+# 
+# # Use IUCN Red List synonyms to match names
+# # Need IUCN Red List key to access API
+# t_complete <- t[rowSums(is.na(t)) == 0,]
+# t_no_match <- t[rowSums(is.na(t)) > 0,]
+# sum(!is.na(t_no_match$name_iucn))
+# 
+# pb <- progress_bar$new(
+#   format = "[:bar] :percent in :elapsed with :eta remaining",
+#   total = nrow(t_no_match), clear = FALSE, width= 60)
+# for(i in 1:nrow(t_no_match)){
+#   syn <- rredlist::rl_synonyms(name = t_no_match$name_iucn[i],
+#                                key = IUCN_REDLIST_KEY,
+#                                parse = TRUE)$result
+#   new_name_phylacine <- ifelse(length(syn)==0,
+#                                t_no_match$name_phylacine[i],
+#                                syn$synonym[which(syn$synonym %in% NM$name_phylacine)])
+#   new_name_pacifici <- ifelse(length(syn)==0,
+#                               t_no_match$name_pacifici[i],
+#                               syn$synonym[which(syn$synonym %in% LH$name_pacifici)])
+#   t_no_match$name_phylacine[i] <- ifelse(is.na(t_no_match$name_phylacine[i]),
+#                                          new_name_phylacine,
+#                                          t_no_match$name_phylacine[i])
+#   t_no_match$name_pacifici[i] <- ifelse(is.na(t_no_match$name_pacifici[i]),
+#                                         new_name_pacifici,
+#                                         t_no_match$name_pacifici[i])
+#   pb$tick()
+# }
+# 
+# # Add newly matched names to list of complete names, keep unmatched names
+# t_new_names <- t_no_match[rowSums(is.na(t_no_match)) == 0,]
+# t_complete <- bind_rows(t_complete, t_new_names)
+# t_no_match <- t_no_match[rowSums(is.na(t_no_match)) > 0,]
+# sum(!is.na(t_no_match$name_iucn))
+# 
+# # Save files
+# write.csv(t_complete, file = paste0(out_dir, 'mammal-names-matched.csv'), row.names = FALSE)
+# write.csv(t_no_match, file = paste0(out_dir, 'mammal-names-no-match.csv'), row.names = FALSE)
+# write.csv(NM, file = paste0(out_dir, 'mammal-names-phylacine.csv'), row.names = FALSE)
+# write.csv(LH, file = paste0(out_dir, 'mammal-names-pacifici.csv'), row.names = FALSE)
 
 
 # Create master names list and join to spatial data ------------------------------------
@@ -176,14 +181,14 @@ master_names_sf <- right_join(MAM_data, master_names, by = "name_iucn")
 # # Mammal range data one by one ----------
 
 # Save out MAM species names in case you want them without reading in the whole thing.
-MAM.unique.ids <- unique(master_df$id_no)
+MAM.unique.ids <- unique(master_names_sf$id_no)
 
 save(MAM.unique.ids, file = paste0(out_dir, 'range-mammal/MAM-ids.rda'))
 
 # Takes a 40 min or so
 for (i in 1:length(MAM.unique.ids)) {
   print(paste0("Currently on ", i, " out of ", length(MAM.unique.ids)))
-  tmp <- MAM_data %>%
+  tmp <- master_names_sf %>%
     filter(id_no == MAM.unique.ids[i])
   st_write(tmp, paste0(out_dir, 'range-mammal/', MAM.unique.ids[i], '.shp'),
            driver = 'ESRI Shapefile', quiet = TRUE, append = FALSE)
