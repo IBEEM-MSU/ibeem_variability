0d89d5e4914ef7020dfb3b5419832b63676d88a0
adrianauscanga <adrianau@uoregon.edu>
2024-01-22 17:48:52 -0500

    remove duplicates from data set and tree

--- Scripts/7-figures/figure_phylogeny.R
+++ Scripts/7-figures/figure_phylogeny.R
@@ -23,18 +23,51 @@ head(bird_data)
 bird_data %>%
   select(ID, Birdtree_name, Avonet_name, Family, GenLength, Modeled_max_longevity) 
 
-# Extract the tree tip labels (in order), use to reorder dataframe
-spp <- bird.phylo$tip.label
+# Extract spp list from tree and from bird_data
+spp_tree <- bird.phylo$tip.label
+spp_data <- bird_data$Birdtree_name
 
 # Change to lower case and remove underscore so that names match "Birdtree_name" in bird dataset
-spp <- sub("_", " ", spp)
-spp <- tolower(spp)
+spp_tree <- sub("_", " ", spp_tree)
+spp_tree <- tolower(spp_tree)
 
-# Make spp data frame with spp names and ordered ID
-spp_df <- as.data.frame(spp) %>%
+# Make spp data frames with spp names and ordered ID
+spp_tree_df <- as.data.frame(spp_tree) %>%
   mutate(ID_spp = seq(1:9993)) %>%
-  mutate(spp_match = spp) %>%
-  select(-spp)
+  mutate(spp_match = spp_tree) %>%
+  select(-spp_tree)
+
+spp_data_df <- as.data.frame(spp_data) %>%
+  mutate(spp_match = spp_data) %>%
+  select(-spp_data)
+
+# Inner join
+spp_list <- inner_join(spp_tree_df, spp_data_df)
+
+# Anti join to get a list of spp that need to be removed from tree
+spp_rm <- anti_join(spp_tree_df, spp_list)
+spp_rm_list <- as.list(spp_rm)
+spp_rm_list <- spp_rm_list[["spp_match"]]
+
+# Remove spp with missing info from both tree and bird_data
+
+bird.phylo$tip.label <- as.character(spp_tree)
+bird_tree <- ape::drop.tip(bird.phylo, spp_rm_list)
+
+# Extract tip labels from tree (in order)
+spp_match <- bird_tree$tip.label
+
+spp_match_df <- as.data.frame(spp_match) %>%
+  mutate(ID_spp = seq(1:9648))
+
+# Add bird data to spp list
+bird_data_short <- bird_data %>%
+  mutate(spp_match = as.character(Birdtree_name)) %>%
+  select(ID, spp_match, Family, GenLength) %>%
+  inner_join(spp_match_df)
+
+bird_data_short <- bird_data_short %>%
+  filter(!duplicated(ID_spp))
 
 # Remove spp that are not in bird dataset from tree
 bird_data <- bird_data %>%
