64decb5c8714ee050e424a0fd96bda3704c9dd71
kellykapsar <kelly.kapsar@gmail.com>
2024-03-14 18:58:51 -0500

    Update script for KEK account.

--- Scripts/1-clean-data/1c-create-batch-scripts.sh
+++ Scripts/1-clean-data/1c-create-batch-scripts.sh
@@ -9,7 +9,7 @@ echo "#!/bin/bash
 #SBATCH -c 2 #cpus
 #SBATCH --time=3:59:00
 #SBATCH --mail-type=FAIL,END
-#SBATCH --mail-user=ccy@msu.edu
+#SBATCH --mail-user=kapsarke@msu.edu
 #SBATCH --mem=50G #memory requested
 #SBATCH --output=%x-%j.SLURMout
 #SBATCH --constraint=amd22
@@ -22,9 +22,6 @@ module load GCC/8.3.0
 module load OpenMPI/3.1.4
 module load R/4.1.0
 
-#manually specify R lib
-export R_LIBS=/mnt/home/ccy/R/x86_64-pc-linux-gnu-library/4.1
-
-Rscript /mnt/home/ccy/variability/Scripts/1-clean-data/1c-process-ncdf-monthly.R /mnt/research/ibeem/variability/data/L0/climate/era5/ /mnt/research/ibeem/variability/data/L1/climate/era5/ $i
+Rscript ./Scripts/1-clean-data/1c-process-ncdf-monthly.R /mnt/research/ibeem/variability_testing/data/L0/climate/era5/ /mnt/research/ibeem/variability_testing/data/L1/climate/era5/ $i
 " > "chunks/1c-chunk-$i.slurm"
 done
