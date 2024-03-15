11bf386d48f5a518109301f5ee4459feb68535f0
kellykapsar <kelly.kapsar@gmail.com>
2024-03-14 22:17:29 -0400

    Update file paths for code testing.

--- Scripts/0-config.R
+++ Scripts/0-config.R
@@ -5,4 +5,4 @@
 # OVERVIEW:         File to specify environmental variables used across multiple scripts (e.g., dir). This file will be sourced in each code file to avoid the necessity of repeatedly specifying directories, dates, etc. 
 
 # Specify data directory 
-dir='/mnt/research/ibeem/variability/'
+dir='/mnt/research/ibeem/variability_testing/'
