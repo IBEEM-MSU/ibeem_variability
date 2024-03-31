0ba3be2aadfd019146ce69180133460d39aaf301
kellykapsar <kelly.kapsar@gmail.com>
2024-03-31 15:06:10 -0500

    Update author order on branch file.

--- Scripts/0-config.R
+++ Scripts/0-config.R
@@ -1,6 +1,6 @@
 # TITLE:            Configuration File
 # PROJECT:          IBEEM Environmental Variability and Life History
-# AUTHORS:          Casey Youngflesh, Peter Williams, Jeff Dozer, Adriana Uscanga, Lala Kounta, Kelly Kapsar, Phoebe Zarnetske, Pat Bills
+# AUTHORS:          Casey Youngflesh, Adriana Uscanga, Peter J. Williams, Jeffrey W. Doser, Lala Kounta, Kelly Kapsar, Phoebe L. Zarnetske
 # DATE:             March 2024 
 # OVERVIEW:         File to specify environmental variables used across multiple scripts (e.g., dir). This file will be sourced in each code file to avoid the necessity of repeatedly specifying directories, dates, etc. 
 
