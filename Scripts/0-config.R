d04664ec4217b77c02791755b2e7d80e69a6eba6
Casey Youngflesh <caseyyoungflesh@gmail.com>
2024-09-08 12:25:22 -0400

    change config

--- Scripts/0-config.R
+++ Scripts/0-config.R
@@ -5,8 +5,9 @@
 # OVERVIEW:         File to specify environmental variables used across multiple scripts (e.g., dir). This file will be sourced in each code file to avoid the necessity of repeatedly specifying directories, dates, etc. 
 
 # Specify data directory
-dir='/mnt/research/ibeem/variability_testing/'
+# dir='/mnt/research/ibeem/variability_testing/'
 # dir='/mnt/research/ibeem/variability/'
+dir='~/Work/Research/Projects/IBEEM_variability/'
 
 # Specify model run date (for step 6 and beyond)
 gl_run_date <- '2024-04-14'
